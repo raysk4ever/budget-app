d2535ec764632ca55c883451f01479e347f5f05f
Ravi Singh <raysk7161@gmail.com>
2024-05-26 12:00:57 +0530

    added: DB connection method

---
+++ src/app/api/db.ts
@@ -0,0 +1,13 @@
+import {MongoClient} from 'mongodb'
+
+const URI = process.env.DATABASE_URI as string
+
+export const connect = async () => {
+    const client = await MongoClient.connect(URI)
+    console.log('connect to mongodb.')
+    const db = client.db()
+    return {
+      client,
+      db
+    }
+}
