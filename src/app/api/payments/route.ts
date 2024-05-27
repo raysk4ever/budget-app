ccaf54bac82b326bffbcd9e7e90a280dbe4767d6
Ravi <Ravikant.Singh@externals.adidas.com>
2024-05-27 16:14:38 +0530

    remove search params

--- src/app/api/payments/route.ts
+++ src/app/api/payments/route.ts
@@ -4,9 +4,9 @@ import { connect, COLLECTIONS } from '../db'
 export async function GET(req: NextRequest) {
     try {
         const {db} = await connect()
-        const searchParams = req.nextUrl.searchParams
-        const perPage = +(searchParams.get('perPage') ?? 10);
-        const pageNo = +(searchParams.get('pageNo') ?? 0);
+        // const searchParams = req.nextUrl.searchParams
+        const perPage = 10 // +(searchParams.get('perPage') ?? 10);
+        const pageNo = 0 +(searchParams.get('pageNo') ?? 0);
 
         const payments = await db.collection(COLLECTIONS.PAYMENTS).find({}).limit(perPage).skip(perPage * pageNo).toArray()
 
@@ -14,6 +14,7 @@ export async function GET(req: NextRequest) {
             message: 'Successfully fetched the payments',
             payments,
         })
+
     } catch (error: any) {
         return Response.json({
             error: error.message
