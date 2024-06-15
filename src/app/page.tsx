9c541bf3c8350fdfb4f16144d9fadd042be8f59a
Ravi <Ravikant.Singh@externals.adidas.com>
2024-06-16 00:22:42 +0530

    fixed issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
 "use client";
 
-import AddExpense from "./components/add-expense";
-import ExpenseList from "./components/expense-list";
-import Row from "./components/row";
-import "../styles/common.css";
-import Navbar from "./component-ui/navbar";
-import Sidebar from "./component-ui/sidebar";
-import Content from "./component-ui/content";
-
-import styles from "../styles/page.module.css";
-import styled from "styled-components";
 import Home from "./home/page";
+import "../styles/common.css";
 
 export default function Page() {
   return (
